84eea30953a41aca8513849edb9a5289304aa5f5
thomasjones <tgjones@byu.edu>
2025-03-12 16:08:07 -0600

    AuthDAO test is complete

--- server/src/test/java/dataaccess/AuthDAOTest.java
+++ server/src/test/java/dataaccess/AuthDAOTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.ClearService;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AuthDAOTest {
     private ClearService clearService;
@@ -28,5 +27,55 @@ public class AuthDAOTest {
         assertThrows(DataAccessException.class, () -> AuthDAO.createAuth("user","12345"));//Error returns since it will only create AuthTokens to Users that exist in DB
     }
 
+    @Test void getUsernameWithRealAuthToken() throws DataAccessException{
+        AuthDAO.createAuth("testUser","12345");
 
+        String result = AuthDAO.getUsername("12345");
+        assertEquals("testUser", result);// result should == "testUser"
+
+
+    }
+
+    @Test void getUsernameWithFakeAuthToken() throws DataAccessException{
+        AuthDAO.createAuth("testUser","12345");
+
+        String result = AuthDAO.getUsername("15");
+        assertNull(result);//result should be null if no user is returned.
+
+
+    }
+
+    @Test void deleteAuthThatExists() throws DataAccessException{
+        AuthDAO.createAuth("testUser","12345");
+        assertDoesNotThrow( () -> AuthDAO.deleteAuth("12345"));
+    }
+
+    @Test
+    public void testDeleteAuthWithNonExistentToken() throws DataAccessException {
+        assertDoesNotThrow(() -> AuthDAO.deleteAuth("nonExistentToken"));
+    }
+
+    @Test void testIsValidAuthTokenWithValidAuth() throws DataAccessException{
+        AuthDAO.createAuth("testUser","12345");
+
+        boolean result = AuthDAO.isValidAuth("12345");
+        assertTrue(result);
+
+    }
+
+    @Test void testIsValidAuthTokenWithFakeAuth() throws DataAccessException{
+        AuthDAO.createAuth("testUser","12345");
+
+        boolean result = AuthDAO.isValidAuth("15");
+        assertFalse(result);
+
+    }
+
+    @Test
+    public void clearNormal() throws DataAccessException{
+        AuthDAO.createAuth("testUser","12345");
+
+        assertDoesNotThrow( () -> AuthDAO.clear(),"Lack of error indicates clear was successful");
+
+    }
 }
