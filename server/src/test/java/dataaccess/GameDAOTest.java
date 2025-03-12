cea0b00db497ef79dd9bd9efcf1324a7198445e6
thomasjones <tgjones@byu.edu>
2025-03-12 16:43:11 -0600

    GameDAO Test cases are complete

--- server/src/test/java/dataaccess/GameDAOTest.java
+++ server/src/test/java/dataaccess/GameDAOTest.java
@@ -1,9 +1,13 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.GameData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.ClearService;
+
+import java.sql.SQLDataException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -46,6 +50,57 @@ public class GameDAOTest {
         GameData gameData = GameDAO.getGame(10000);
 
         assertNull(gameData, "gameData will be null if the gameID is not in DB");
+    }
+
+    @Test
+    public void listGamesSuccess() throws DataAccessException {
+        GameDAO.createGame("TestGame1");
+        GameDAO.createGame("TestGame2");
+
+        List<GameData> games = GameDAO.listGames();
+
+        assertNotNull(games, "The returned game list should not be null");
+        assertEquals("TestGame1", games.get(0).gameName(), "First game name should match");
+        assertEquals("TestGame2", games.get(1).gameName(), "Second game name should match");
+    }
+
+    @Test
+    public void listGamesWithNoGamesInDB() throws DataAccessException{
+        GameDAO.clear();
+
+        List<GameData> games = GameDAO.listGames();
+
+        assertNotNull(games,"Game list should not be be null");
+        assertTrue(games.isEmpty(),"Game list should be empty tho");
+    }
+
+    @Test
+    public void UpdateGameNormal()throws DataAccessException {
+        UserDAO.createUser("playerWhite","test123","test@gmail.com");
+        UserDAO.createUser("playerBlack","test123","test@gmail.com");
+        int gameID = GameDAO.createGame("testGame");
+
+        GameData updatedGame = new GameData(gameID, "playerWhite", "playerBlack", "NonExistentGame", null);
+
+        assertDoesNotThrow( () -> GameDAO.updateGame(updatedGame), "Updating should not throw a DataAccessException");
+    }
+
+    @Test
+    public void UpdateGameWithUsersNotInDB()throws DataAccessException{
+        int gameID = GameDAO.createGame("testGame");
+
+        GameData updatedGame = new GameData(gameID, "playerWhite", "playerBlack", "NonExistentGame", null);
+
+        assertThrows(DataAccessException.class, () -> GameDAO.updateGame(updatedGame), "Updating should DataAccessExceptioin");
+
+    }
+
+    @Test
+    public void clearNormal() throws DataAccessException{
+        GameDAO.createGame("testGame");
+
+        assertDoesNotThrow( () -> GameDAO.clear(),"Lack of error indicates clear was successful");
+
     }
 
 }
