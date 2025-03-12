aa275605fa8192590bd6aaeea24bee9dbac2c183
thomasjones <tgjones@byu.edu>
2025-03-12 15:41:10 -0600

    UserDAO test now complete and passing all tests

--- server/src/test/java/dataaccess/AuthDAOTest.java
+++ server/src/test/java/dataaccess/AuthDAOTest.java
@@ -1,4 +1,15 @@
 package dataaccess;
 
+import org.junit.jupiter.api.BeforeEach;
+import service.ClearService;
+
 public class AuthDAOTest {
+    private ClearService clearService;
+
+    @BeforeEach
+    public void setup() {
+        clearService = new ClearService();
+        clearService.clear();
+
+    }
 }
