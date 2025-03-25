bda33d8e531c83c42a38f41de4471fee1a2f8b4c
thomasjones <tgjones@byu.edu>
2025-03-24 21:38:59 -0600

    regiser test passing

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,31 @@
 package client;
 
+import model.AuthData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import net.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
+
+        var serverUrl = "http://localhost:8080";
+        serverFacade = new ServerFacade(serverUrl);
+    }
+    @Test
+    void register() throws Exception {
+        AuthData authData = serverFacade.register("player1", "password", "p1@email.com");
+        assertTrue(authData.authToken().length() > 10);
     }
 
     @AfterAll
@@ -23,7 +36,7 @@ public class ServerFacadeTests {
 
     @Test
     public void sampleTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
 }
